aa4aebd4df799ac959ce9f667cd518ca43ea2cca
Liam <liam.tab@gmail.com>
2016-02-19 20:37:45 -0800

    More ACPI AML opcodes mapped
    Mapped all of the Type-1 and Type-2 Opcodes from the ACPI rev 5.0 spec.

--- src/hw/acpi/aml/Interpreter.cpp
+++ src/hw/acpi/aml/Interpreter.cpp
@@ -19,32 +19,6 @@ void HW::ACPI::AML::Interpreter :: Init ()
 	OpTable [ 0xA4 ] = & ReturnOp;
 	OpTable [ 0xA5 ] = & BreakOp;
 	
-	// Type 2 op-codes
-	
-	OpTable [ 0x11 ] = & BufferOp;
-	OpTable [ 0x72 ] = & AddOp;
-	OpTable [ 0x73 ] = & ConcatOp;
-	OpTable [ 0x7B ] = & AndOp;
-	OpTable [ 0x84 ] = & ConcatResOp;
-	OpTable [ 0x ] = & ;
-	OpTable [ 0x ] = & ;
-	OpTable [ 0x ] = & ;
-	OpTable [ 0x ] = & ;
-	OpTable [ 0x ] = & ;
-	OpTable [ 0x ] = & ;
-	OpTable [ 0x ] = & ;
-	OpTable [ 0x ] = & ;
-	OpTable [ 0x ] = & ;
-	OpTable [ 0x ] = & ;
-	OpTable [ 0x ] = & ;
-	OpTable [ 0x ] = & ;
-	OpTable [ 0x ] = & ;
-	OpTable [ 0x ] = & ;
-	OpTable [ 0x ] = & ;
-	OpTable [ 0x ] = & ;
-	OpTable [ 0x ] = & ;
-	OpTable [ 0x ] = & ;
-	
 	// Type 1 extended op-codes
 	
 	ExtOpTable [ 0x20 ] = & LoadOp;
@@ -56,31 +30,60 @@ void HW::ACPI::AML::Interpreter :: Init ()
 	ExtOpTable [ 0x2A ] = & UnloadOp;
 	ExtOpTable [ 0x32 ] = & FatalOp;
 	
+	// Type 2 op-codes
+	
+	OpTable [ 0x11 ] = & BufferOp;
+	OpTable [ 0x12 ] = & PackageOp;
+	OpTable [ 0x13 ] = & VarPackageOp;
+	OpTable [ 0x70 ] = & StoreOp;
+	OpTable [ 0x71 ] = & RefOfOp;
+	OpTable [ 0x72 ] = & AddOp;
+	OpTable [ 0x73 ] = & ConcatOp;
+	OpTable [ 0x74 ] = & SubtractOp;
+	OpTable [ 0x75 ] = & IncrementOp;
+	OpTable [ 0x76 ] = & DecrementOp;
+	OpTable [ 0x77 ] = & MultiplyOp;
+	OpTable [ 0x78 ] = & DivideOp;
+	OpTable [ 0x79 ] = & ShiftLeftOp;
+	OpTable [ 0x7A ] = & ShiftRightOp; // ----
+	OpTable [ 0x7B ] = & AndOp;
+	OpTable [ 0x7C ] = & NandOp;
+	OpTable [ 0x7D ] = & OrOp;
+	OpTable [ 0x7E ] = & NorOp;
+	OpTable [ 0x75 ] = & XorOp;
+	OpTable [ 0x80 ] = & NotOp;
+	OpTable [ 0x81 ] = & FindSetLeftBitOp;
+	OpTable [ 0x82 ] = & FindSetRightBitOp;
+	OpTable [ 0x83 ] = & DerefOfOp;
+	OpTable [ 0x84 ] = & ConcatResOp;
+	OpTable [ 0x85 ] = & ModOp;
+	OpTable [ 0x87 ] = & SizeOfOp;
+	OpTable [ 0x88 ] = & IndexOp;
+	OpTable [ 0x89 ] = & MatchOp;
+	OpTable [ 0x8E ] = & ObjectTypeOp;
+	OpTable [ 0x90 ] = & LAndOp;
+	OpTable [ 0x91 ] = & LOrOp;
+	OpTable [ 0x92 ] = & LNotOp;
+	OpTable [ 0x93 ] = & LEqualOp;
+	OpTable [ 0x94 ] = & LGreaterOp;
+	OpTable [ 0x95 ] = & LLessOp;
+	OpTable [ 0x96 ] = & ToBufferOp;
+	OpTable [ 0x97 ] = & ToDecimalStringOp;
+	OpTable [ 0x98 ] = & ToHexStringOp;
+	OpTable [ 0x99 ] = & ToIntegerOp;
+	OpTable [ 0x9C ] = & ToStringOp;
+	OpTable [ 0x9D ] = & CopyObjectOp;
+	OpTable [ 0x9E ] = & MidOp;
+	
 	// Type 2 extended op-codes
 	
 	ExtOpTable [ 0x12 ] = & CondRefOfOp;
 	ExtOpTable [ 0x23 ] = & AcquireOp;
-	ExtOpTable [ 0x ] = & ;
-	ExtOpTable [ 0x ] = & ;
-	ExtOpTable [ 0x ] = & ;
-	ExtOpTable [ 0x ] = & ;
-	ExtOpTable [ 0x ] = & ;
-	ExtOpTable [ 0x ] = & ;
-	ExtOpTable [ 0x ] = & ;
-	ExtOpTable [ 0x ] = & ;
-	ExtOpTable [ 0x ] = & ;
-	ExtOpTable [ 0x ] = & ;
-	ExtOpTable [ 0x ] = & ;
-	ExtOpTable [ 0x ] = & ;
-	ExtOpTable [ 0x ] = & ;
-	ExtOpTable [ 0x ] = & ;
-	ExtOpTable [ 0x ] = & ;
-	ExtOpTable [ 0x ] = & ;
-	ExtOpTable [ 0x ] = & ;
-	ExtOpTable [ 0x ] = & ;
-	ExtOpTable [ 0x ] = & ;
-	ExtOpTable [ 0x ] = & ;
-	ExtOpTable [ 0x ] = & ;
+	ExtOpTable [ 0x28 ] = & FromBCDOp;
+	ExtOpTable [ 0x1F ] = & LoadTableOp;
+	ExtOpTable [ 0x33 ] = & TimerOp;
+	ExtOpTable [ 0x29 ] = & ToBCDOp;
+	ExtOpTable [ 0x25 ] = & WaitOp;
 	
 };
 
@@ -168,7 +171,7 @@ void HW::ACPI::AML::Interpreter :: WhileOp ( InterpreterContext * Context ) // 0
 };
 
 // ===============
-// Type 1 opcodes.
+// Type 2 opcodes.
 // ===============
 
 void HW::ACPI::AML::Interpreter :: AddOp ( InterpreterContext * Context ) // 0x72
@@ -206,112 +209,259 @@ void HW::ACPI::AML::Interpreter :: ConcatResOp ( InterpreterContext * Context ) 
 	
 };
 
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
+void HW::ACPI::AML::Interpreter :: CopyObjectOp ( InterpreterContext * Context ) // 0x9D
 {
 	
 	
 	
 };
 
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
+void HW::ACPI::AML::Interpreter :: DecrementOp ( InterpreterContext * Context ) // 0x76
 {
 	
 	
 	
 };
 
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
+void HW::ACPI::AML::Interpreter :: DerefOfOp ( InterpreterContext * Context ) // 0x83
 {
 	
 	
 	
 };
 
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
+void HW::ACPI::AML::Interpreter :: DivideOp ( InterpreterContext * Context ) // 0x78
 {
 	
 	
 	
 };
 
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
+void HW::ACPI::AML::Interpreter :: FindSetLeftBitOp ( InterpreterContext * Context ) // 0x81
 {
 	
 	
 	
 };
 
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
+void HW::ACPI::AML::Interpreter :: FindSetRightBitOp ( InterpreterContext * Context ) // 0x82
 {
 	
 	
 	
 };
 
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
+void HW::ACPI::AML::Interpreter :: IncrementOp ( InterpreterContext * Context ) // 0x75
 {
 	
 	
 	
 };
 
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
+void HW::ACPI::AML::Interpreter :: IndexOp ( InterpreterContext * Context ) // 0x88
 {
 	
 	
 	
 };
 
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
+void HW::ACPI::AML::Interpreter :: LAndOp ( InterpreterContext * Context ) // 0x90
 {
 	
 	
 	
 };
 
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
+void HW::ACPI::AML::Interpreter :: LEqualOp ( InterpreterContext * Context ) // 0x93
 {
 	
 	
 	
 };
 
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
+void HW::ACPI::AML::Interpreter :: LGreaterOp ( InterpreterContext * Context ) // 0x94
 {
 	
 	
 	
 };
 
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
+void HW::ACPI::AML::Interpreter :: LLessOp ( InterpreterContext * Context ) // 0x95
 {
 	
 	
 	
 };
 
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
+void HW::ACPI::AML::Interpreter :: LNotOp ( InterpreterContext * Context ) // 0x92
 {
 	
 	
 	
 };
 
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
+void HW::ACPI::AML::Interpreter :: LOrOp ( InterpreterContext * Context ) // 0x91
 {
 	
 	
 	
 };
 
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
+void HW::ACPI::AML::Interpreter :: MatchOp ( InterpreterContext * Context ) // 0x89
 {
 	
 	
 	
 };
 
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
+void HW::ACPI::AML::Interpreter :: MidOp ( InterpreterContext * Context ) // 0x9E
+{
+	
+	
+	
+};
+
+void HW::ACPI::AML::Interpreter :: ModOp ( InterpreterContext * Context ) // 0x85
+{
+	
+	
+	
+};
+
+void HW::ACPI::AML::Interpreter :: MultiplyOp ( InterpreterContext * Context ) // 0x77
+{
+	
+	
+	
+};
+
+void HW::ACPI::AML::Interpreter :: NandOp ( InterpreterContext * Context ) // 0x7C
+{
+	
+	
+	
+};
+
+void HW::ACPI::AML::Interpreter :: NorOp ( InterpreterContext * Context ) // 0x7E
+{
+	
+	
+	
+};
+
+void HW::ACPI::AML::Interpreter :: NotOp ( InterpreterContext * Context ) // 0x80
+{
+	
+	
+	
+};
+
+void HW::ACPI::AML::Interpreter :: ObjectTypeOp ( InterpreterContext * Context ) // 0x8E
+{
+	
+	
+	
+};
+
+void HW::ACPI::AML::Interpreter :: OrOp ( InterpreterContext * Context ) // 0x7D
+{
+	
+	
+	
+};
+
+void HW::ACPI::AML::Interpreter :: PackageOp ( InterpreterContext * Context ) // 0x12
+{
+	
+	
+	
+};
+
+void HW::ACPI::AML::Interpreter :: VarPackageOp ( InterpreterContext * Context ) // 0x13
+{
+	
+	
+	
+};
+
+void HW::ACPI::AML::Interpreter :: RefOfOp ( InterpreterContext * Context ) // 0x71
+{
+	
+	
+	
+};
+
+void HW::ACPI::AML::Interpreter :: ShiftLeftOp ( InterpreterContext * Context ) // 0x79
+{
+	
+	
+	
+};
+
+void HW::ACPI::AML::Interpreter :: ShiftRightOp ( InterpreterContext * Context ) // 0x7A
+{
+	
+	
+	
+};
+
+void HW::ACPI::AML::Interpreter :: SizeOfOp ( InterpreterContext * Context ) // 0x87
+{
+	
+	
+	
+};
+
+void HW::ACPI::AML::Interpreter :: StoreOp ( InterpreterContext * Context ) // 0x70
+{
+	
+	
+	
+};
+
+void HW::ACPI::AML::Interpreter :: SubtractOp ( InterpreterContext * Context ) // 0x74
+{
+	
+	
+	
+};
+
+void HW::ACPI::AML::Interpreter :: ToBufferOp ( InterpreterContext * Context ) // 0x96
+{
+	
+	
+	
+};
+
+void HW::ACPI::AML::Interpreter :: ToDecimalStringOp ( InterpreterContext * Context ) // 0x97
+{
+	
+	
+	
+};
+
+void HW::ACPI::AML::Interpreter :: ToHexStringOp ( InterpreterContext * Context ) // 0x98
+{
+	
+	
+	
+};
+
+void HW::ACPI::AML::Interpreter :: ToIntegerOp ( InterpreterContext * Context ) // 0x99
+{
+	
+	
+	
+};
+
+void HW::ACPI::AML::Interpreter :: ToStringOp ( InterpreterContext * Context ) // 0x9C
+{
+	
+	
+	
+};
+
+void HW::ACPI::AML::Interpreter :: XorOp ( InterpreterContext * Context ) // 0x7F
 {
 	
 	
@@ -396,105 +546,35 @@ void HW::ACPI::AML::Interpreter :: CondRefOfOp ( InterpreterContext * Context ) 
 	
 };
 
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
+void HW::ACPI::AML::Interpreter :: FromBCDOp ( InterpreterContext * Context ) // Ext 0x28
 {
 	
 	
 	
 };
 
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
+void HW::ACPI::AML::Interpreter :: LoadTableOp ( InterpreterContext * Context ) // Ext 0x1F
 {
 	
 	
 	
 };
 
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
+void HW::ACPI::AML::Interpreter :: TimerOp ( InterpreterContext * Context ) // Ext 0x33
 {
 	
 	
 	
 };
 
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
+void HW::ACPI::AML::Interpreter :: ToBCDOp ( InterpreterContext * Context ) // Ext 0x29
 {
 	
 	
 	
 };
 
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
-{
-	
-	
-	
-};
-
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
-{
-	
-	
-	
-};
-
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
-{
-	
-	
-	
-};
-
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
-{
-	
-	
-	
-};
-
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
-{
-	
-	
-	
-};
-
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
-{
-	
-	
-	
-};
-
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
-{
-	
-	
-	
-};
-
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
-{
-	
-	
-	
-};
-
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
-{
-	
-	
-	
-};
-
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
-{
-	
-	
-	
-};
-
-void HW::ACPI::AML::Interpreter :: _Op ( InterpreterContext * Context )
+void HW::ACPI::AML::Interpreter :: WaitOp ( InterpreterContext * Context ) // Ext 0x25
 {
 	
 	
